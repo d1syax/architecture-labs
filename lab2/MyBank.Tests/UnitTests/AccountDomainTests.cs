8f0466fbd67e9dd73e5d46c3c4694f37d72edb26
= <boyaka201@gmail.com>
2026-04-29 12:25:17 +0300

    test: add domain unit tests for Account and User without database

---
+++ lab2/MyBank.Tests/UnitTests/AccountDomainTests.cs
@@ -0,0 +1,72 @@
+using MyBank.Domain.Models;
+
+namespace MyBank.Tests.UnitTests;
+
+public class AccountDomainTests
+{
+    [Fact]
+    public void CreateAccount_ValidCurrency_ReturnsAccount()
+    {
+        var (account, error) = Account.Create(1, "USD");
+
+        Assert.Null(error);
+        Assert.NotNull(account);
+        Assert.Equal("USD", account.Currency);
+        Assert.Equal(0, account.Balance);
+    }
+
+    [Fact]
+    public void CreateAccount_InvalidCurrency_ReturnsDomainError()
+    {
+        var (account, error) = Account.Create(1, "XYZ");
+
+        Assert.Null(account);
+        Assert.NotNull(error);
+        Assert.Equal("INVALID_CURRENCY", error.Code);
+    }
+
+    [Fact]
+    public void Debit_SufficientFunds_UpdatesBalance()
+    {
+        var (account, _) = Account.Create(1, "USD");
+        account!.Credit(1000);
+
+        var error = account.Debit(300);
+
+        Assert.Null(error);
+        Assert.Equal(700, account.Balance);
+    }
+
+    [Fact]
+    public void Debit_InsufficientFunds_ReturnsDomainError()
+    {
+        var (account, _) = Account.Create(1, "USD");
+        account!.Credit(100);
+
+        var error = account.Debit(500);
+
+        Assert.NotNull(error);
+        Assert.Equal("INSUFFICIENT_FUNDS", error.Code);
+    }
+
+    [Fact]
+    public void Debit_NegativeAmount_ReturnsDomainError()
+    {
+        var (account, _) = Account.Create(1, "USD");
+
+        var error = account!.Debit(-100);
+
+        Assert.NotNull(error);
+        Assert.Equal("INVALID_AMOUNT", error.Code);
+    }
+
+    [Fact]
+    public void Credit_ValidAmount_UpdatesBalance()
+    {
+        var (account, _) = Account.Create(1, "USD");
+
+        account!.Credit(500);
+
+        Assert.Equal(500, account.Balance);
+    }
+}
